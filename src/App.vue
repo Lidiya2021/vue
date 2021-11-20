3b746286f3134ec9f9e64b5f7cce6798eaff0ecc
Lidiya2021 <suliz2009@rambler.ru>
2021-11-20 11:20:06 +0300

    lessons-1

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+   <img alt="Vue logo" src="./assets/logo.png"> 
+   <HelloWorld msg="" :is-show="true"/>
+    <Calc />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Calc from './components/Calc.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Calc,
+  },
+  data() {
+    return {
+      counter: 0,
+      str: 'Hello!',
+    };
+  },
+  methods: {
+    increase(step, event) {
+      console.log(event);
+      this.counter += step;
+    },
+    resetCounter() {
+      this.counter = 0;
+    },
+    input(event) {
+      console.log(event.target.value);
+      this.str = event.target.value;
+    },
+    blur() {
+      console.log('blur');
+    },
+    focus() {
+      console.log('focus');
+    },
+  },
+};
+</script>
+
+<style lang="css">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
